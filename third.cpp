02cfeb84f5d5eca3fbd1130a1dae2e09cfe221b1
Arnav9340 <97521072+Arnav9340@users.noreply.github.com>
2025-02-03 14:09:49 +0530

    third file changed

---
+++ third.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+int32_t main()
+{
+
+    int t;
+    cin>>t;
+    while (t--)
+    {
+    }
+
+    return 0;
+}
