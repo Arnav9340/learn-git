7c4b0fc4c4ba800616e2b5fa93ff3fefe4ca8753
Arnav9340 <97521072+Arnav9340@users.noreply.github.com>
2025-02-03 15:09:14 +0530

    mutliply added

---
+++ multiply.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+int32_t main()
+{
+
+    
+    int a = 5 ;
+    int b = 3 ;
+
+    cout<<a*b<<endl ;
+
+    return 0;
+}
